f957c381f59961920457486f3a241652a2a2b3b3
Nervous <linkbleu2000@hotmail.com>
2015-07-07 02:05:00 +0200

    DLL/Hooking: Added FindFiles functions (Not working yet)

---
+++ GreenKit/FindFiles.h
@@ -0,0 +1,12 @@
+#ifndef FIND_FILES_H
+# define FIND_FILES_H
+
+
+# include "windefs.h"
+
+HANDLE WINAPI MyFindFirstFileA(LPCTSTR lpFileName, LPWIN32_FIND_DATAA lpFindFileData);
+int WINAPI MyFindNextFileA(HANDLE hFindFile, LPWIN32_FIND_DATAA lpFindFileData);
+HANDLE WINAPI MyFindFirstFileW(LPCTSTR lpFileName, LPWIN32_FIND_DATAW lpFindFileData);
+int WINAPI MyFindNextFileW(HANDLE hFindFile, LPWIN32_FIND_DATAW lpFindFileData);
+
+#endif
